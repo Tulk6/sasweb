fda9f8226e860d13f36856cb7376e81dcd7af994
itisacrouton <itisacrouton@proton.me>
2026-06-18 10:50:51 +1000

    fixed updater so when run independently cds to dir

--- updater.py
+++ updater.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import subprocess
+import subprocess, os
 
 def update():
     src = "https://github.com/Tulk6/sasweb.git"
@@ -9,4 +9,7 @@ def update():
     
 
 if __name__ == '__main__':
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dir_path)
     update()
+    input("")
